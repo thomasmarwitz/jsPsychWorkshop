3e352999afa9a223f38804eb348f9a2f87efe895
Thomas Marwitz <thomasmarwitz3@gmail.com>
2021-09-28 21:59:19 +0200

    demo cheatsheet

---
+++ demo.js
@@ -0,0 +1,36 @@
+// declare
+var x
+let y
+const z
+
+// init
+x = 10
+y = "Hello" // verschiedene Typen, implizit, bestimmt die Operationen
+// num + vs. string +
+
+
+// Statements: syntaktische Einheit, die beschreibt, dass eine Operation ausgeführt wird
+// Zuweisung, Call, return
+
+// Compound Statements: if/else, Schleifen
+
+// Expressions
+// Zusammensetzung aus Werten und aus Funktionsaufrufen
+
+
+// Auslagerung von Code, Wiederverwendbarkeit
+function printTree() {
+    console.log("*")
+    console.log("**")
+    console.log("***")
+    console.log("****")
+    console.log("*****")
+}
+// erstmal machen, dann Muster erkennen und auslagern
+
+// wachsen ist ein Prozess: => Schleifen
+
+let s = ""
+for (let i = 0; i < 5; i++) { // Beschreibung + Ausführung
+    
+}
